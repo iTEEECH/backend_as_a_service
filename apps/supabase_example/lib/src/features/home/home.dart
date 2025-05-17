d7a39ac1a46f96e7dea8c8ac196db8e0e689b65b
Florentin Bouchez <florentin.bouchez@gmail.com>
2025-05-17 08:30:52 -0400

    feat: Implement Supabase

---
+++ apps/supabase_example/lib/src/features/home/home.dart
@@ -0,0 +1,5 @@
+export 'presentation/widgets/home_body.dart';
+export 'presentation/widgets/home_sign_out_button.dart';
+export 'presentation/widgets/home_title.dart';
+export 'presentation/widgets/home_welcome.dart';
+export 'presentation/home_page.dart';
---
+++ apps/supabase_example/lib/src/features/sign_in/sign_in.dart
@@ -0,0 +1,12 @@
+export 'presentation/screens/sign_in_desktop_screen.dart';
+export 'presentation/screens/sign_in_mobile_screen.dart';
+export 'presentation/widgets/sign_in_body.dart';
+export 'presentation/widgets/sign_in_button.dart';
+export 'presentation/widgets/sign_in_cover.dart';
+export 'presentation/widgets/sign_in_email_field.dart';
+export 'presentation/widgets/sign_in_forgot_password_button.dart';
+export 'presentation/widgets/sign_in_form.dart';
+export 'presentation/widgets/sign_in_subtitle.dart';
+export 'presentation/widgets/sign_in_password_field.dart';
+export 'presentation/widgets/sign_in_title.dart';
+export 'presentation/sign_in_page.dart';
